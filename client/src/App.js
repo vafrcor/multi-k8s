ed4a6508f3e3efacc90e1902d1d23230947bf6a2
Vinsensius Angelo <vafrcor2007@yahoo.co.id>
2020-02-25 16:51:48 +0800

    change home page wording

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator v.2
+            Fib Calculator Kubernetes
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
